67d619296a7736e870327596c02a5381df90f6f3
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-03-29 16:54:42 -0500

    You guessed: ...

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,11 +3,6 @@ use std::io;
 
 fn main() {
     let mut guess = String::new();
-    let foo = bar;
-
-    // The following example shows how to use mut before the variable name to make a variable mutable:
-    // let foo = 5; // immutable
-    // let mut bar = 5; // mutable
 
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line");
@@ -21,5 +16,7 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    
 }
 
