f5187b2e99fc885295eda38de8b39b0e220963f5
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-03-29 16:46:00 -0500

    third one

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,8 +3,9 @@ use std::io;
 
 fn main() {
     let mut guess = String::new();
-    // let foo = bar;
+    let foo = bar;
 
+    // The following example shows how to use mut before the variable name to make a variable mutable:
     // let foo = 5; // immutable
     // let mut bar = 5; // mutable
 
