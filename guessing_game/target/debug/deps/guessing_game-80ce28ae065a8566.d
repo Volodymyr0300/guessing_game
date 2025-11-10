1a3a29dbb590bdacd3aa9fe418668ebc9af001b8
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-10 11:18:23 -0600

    replacing right code

--- guessing_game/target/debug/deps/guessing_game-80ce28ae065a8566.d
+++ guessing_game/target/debug/deps/guessing_game-80ce28ae065a8566.d
@@ -1,5 +1,5 @@
-/Users/admin/Documents/github/rust/guessing_game/target/debug/deps/libguessing_game-80ce28ae065a8566.rmeta: src/main.rs
+/Users/admin/Documents/rust/guessing_game/target/debug/deps/libguessing_game-80ce28ae065a8566.rmeta: src/main.rs
 
-/Users/admin/Documents/github/rust/guessing_game/target/debug/deps/guessing_game-80ce28ae065a8566.d: src/main.rs
+/Users/admin/Documents/rust/guessing_game/target/debug/deps/guessing_game-80ce28ae065a8566.d: src/main.rs
 
 src/main.rs:
