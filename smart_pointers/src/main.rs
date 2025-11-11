9d19952a4d9fe057166b4b221c4e9797a71a4dd0
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-10 21:28:13 -0600

    impl_deref_trait

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,15 +1,15 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
-    /* 
-    let x = 5;
-    let y = &x;
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-    */
-
-    let x = 5;
-    let y = Box::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    
 }
