54fe8fee9d9e1eace659b28565e0b972fc7713d3
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-11-10 20:50:05 -0600

    Box_like_a_reference

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,13 +1,15 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
-}
-
-use crate::List::{Cons, Nil};
-
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    /* 
+    let x = 5;
+    let y = &x;
 
-    let b = Box::new(5);
-    println!("b = {b}");
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    */
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
